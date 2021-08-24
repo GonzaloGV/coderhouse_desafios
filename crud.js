cac4fd7abbf1d51bdd91e37df5fb177f8edfee5c
gonzalo <gonzalovasquezher@gmail.com>
2021-08-24 20:14:25 -0300

    desafio handlebars completado

--- crud.js
+++ crud.js
@@ -1,6 +1,4 @@
-import * as fs from 'fs';
-
-
+import * as fs from "fs";
 
 class InventoryManager {
   #idCounter;
@@ -8,29 +6,29 @@ class InventoryManager {
 
   constructor() {
     this.#idCounter = 1;
-    this.#productArray = []; 
+    this.#productArray = [];
   }
 
   getProductById(id) {
-    const filteredProducts = this.#productArray.filter(product => product.id === id);
-    if (filteredProducts.length === 0) 
+    const filteredProducts = this.#productArray.filter(
+      (product) => product.id === id
+    );
+    if (filteredProducts.length === 0)
       return {
-      'error': 'producto no encontrado'
-    }
+        error: "producto no encontrado",
+      };
     return filteredProducts[0];
   }
 
   getProducts() {
-    if (this.#productArray.length == 0)
-      return {error: 'no hay productos cargados'}
     return this.#productArray;
   }
 
   addProduct(product) {
     const finalProduct = {
       id: this.#idCounter,
-      ...product
-    }
+      ...product,
+    };
     this.#productArray.push(finalProduct);
     this.#idCounter++;
 
@@ -39,29 +37,28 @@ class InventoryManager {
 
   updateProduct(product) {
     const idx = this.#findIndex(Number(product.id));
-    if (idx === -1) return {'error': 'producto no encontrado'};
+    if (idx === -1) return { error: "producto no encontrado" };
     this.#productArray[idx] = product;
-    
 
     return product;
   }
 
   deleteProduct(id) {
     const idx = this.#findIndex(id);
-    if (idx === -1) return {'error': 'producto no encontrado'};
+    if (idx === -1) return { error: "producto no encontrado" };
     const productDeleted = this.#productArray.splice(idx, 1);
     return productDeleted[0];
   }
 
   #findIndex(id) {
-    return this.#productArray.findIndex(prods => prods.id === id)
+    return this.#productArray.findIndex((prods) => prods.id === id);
   }
-
 }
 
 export class Serializer {
-  static serializeProduct(obj) { return JSON.stringify(obj, null, "\t")};
+  static serializeProduct(obj) {
+    return JSON.stringify(obj, null, "\t");
+  }
 }
 
-export default new  InventoryManager();
-
+export default new InventoryManager();
--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 import express from "express";
-import productos from './routes/productos.route.js';
+import productos from "./routes/productos.route.js";
+import handlebars from "express-handlebars";
 
+const { pathname: __dirname } = new URL("./", import.meta.url);
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'))
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "views/layouts",
+    partialsDir: __dirname + "views/partials/",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+// app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/api/productos', productos);
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/productos", productos);
 
+app.get("/", (request, response) => {
+  response.render("addProduct");
+});
 
-try {
-  app.listen(port, (error) => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-  });
-} catch (error) {
-  console.log("Hubo un error, el servidor no pudo ser inicio");
-  console.log(error);
-}
+app.listen(port, (error) => {
+  if (error) throw new Error(`Error en servidor ${error}`);
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
